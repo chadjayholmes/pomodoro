6a5aea3720611e9860ac31561706eb0e31fcedc7
Chad Holmes <chadjayholmes@gmail.com>
2021-11-06 16:48:55 -0600

    instructional message on bookmarks page wasn't applying margins when there were no summaries. Fixed

--- src/components/MyBookmarks.vue
+++ src/components/MyBookmarks.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="summaries-wrap">
-      <div v-show="bookmarked.length === 0">
+      <div class="message" v-show="bookmarked.length === 0">
         Click the
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-bookmark icon" viewBox="0 0 16 16">
         <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
@@ -101,6 +101,12 @@ export default {
   font-size: 12px;
 }
 
+.message {
+  width: 50%;
+  margin-right: auto;
+  margin-left: auto;
+}
+
 .link-text{
   margin: 0;
 }
@@ -112,6 +118,10 @@ export default {
 
 @media only screen and (max-width: 600px){
   .summary-object{
+    width: 90%;
+  }
+
+  .message {
     width: 90%;
   }
 
