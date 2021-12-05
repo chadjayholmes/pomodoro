ee9786e8e6daae1ffb3571221d1ce4990ac11c5a
Chad Holmes <chadjayholmes@gmail.com>
2021-12-04 20:09:45 -0700

    init categories doc in DB

--- back-end/server.js
+++ back-end/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
 const mongoose = require('mongoose');
 
 // connect to the database
-mongoose.connect('mongodb://127.0.0.1:27017/pomodoro', {
+mongoose.connect('mongodb://127.0.0.1:58561/pomodoro', {
     useNewUrlParser: true
 });
 
