ed55672af0a719191625b33ca50f5d7b71475a9e
Chad Holmes <chadjayholmes@gmail.com>
2021-12-05 00:01:35 -0700

    just fixed one wrong icon on bookmarked page

--- front-end/src/components/MyBookmarks.vue
+++ front-end/src/components/MyBookmarks.vue
@@ -12,13 +12,12 @@
       </div>
       <div v-show="bookmarked.length !== 0">
         Click the
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-bookmark-check icon" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-          <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#3f4a60" class="bi bi-bookmark-check icon" viewBox="0 0 16 16">
+          <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
         </svg>
         icon in
         <router-link class="link-text" to="/MySummaries">All Summaries</router-link>
-        to remove un-bookmark a summary.
+        to un-bookmark a summary.
       </div>
       <div v-for="summary in bookmarked" v-bind:key="summary" class="summary-object">
         <div class="title">
